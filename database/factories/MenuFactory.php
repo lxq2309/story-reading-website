7d8032aa188c6eef23ac6fdad3a0145ad7913f44
Quynh Le <lxq.2309@gmail.com>
2024-04-15 16:16:27 +0100

    create MenuSeeder to generate fake menu data

--- database/factories/MenuFactory.php
+++ database/factories/MenuFactory.php
@@ -17,7 +17,9 @@ class MenuFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name,
+            'description' => fake()->text,
+            'link' => fake()->url,
         ];
     }
 }
