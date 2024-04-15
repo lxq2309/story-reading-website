7a3b91f57ccd44e7ccd339f25eb053a33d28c618
Quynh Le <lxq.2309@gmail.com>
2024-04-15 16:08:04 +0100

    create UserSeeder to generate fake user data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -24,11 +25,20 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'username' => $this->faker->unique()->userName(),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'address' => fake()->address(),
             'password' => static::$password ??= Hash::make('password'),
+            'date_of_birth' => fake()->dateTimeBetween('-30 years', '-18 years'),
+            'avatar' => fake()->text,
+            'description' => fake()->text,
+            'gender' => rand(0, 1),
             'remember_token' => Str::random(10),
+            'created_at' => fake()->dateTimeBetween('-30 years', '-18 years'),
+            'updated_at' => fake()->dateTimeBetween('-30 years', '-18 years'),
+            'role' => rand(0, 2),
         ];
     }
 
